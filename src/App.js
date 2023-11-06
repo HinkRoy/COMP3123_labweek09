1448e7344ab0e775ad45ca9e35d64b8fd0fa3a66
HinkRoy <123959949+HinkRoy@users.noreply.github.com>
2023-11-06 16:34:35 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const studentId = "101304724";
+  const studentName = "Heng Zhou";
+  
+  return (
+    <div className="App">
+    <header className="App-header">
+      
+      <p>Welcome to Fullstack Development - I</p>
+      <p>React JS Programming Week09 Lab exercise</p>
+      <p>101304724</p>
+      <p>HengZhou</p>
+      <p>George Brown College, Toronto</p>
+    </header>
+  </div>
+  );
+}
+
+export default App;
